5500f9c9ebe838798107e42131d14f7f3d2f611a
SeamusLynchTUD <c23471656@mytudublin.ie>
2024-10-13 18:03:57 +0100

    First Player Movement Script

---
+++ Game-Tools-2 Roguelike/Assets/Scripts/Player/PlayerMovementScript.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    public Rigidbody2D body;
+    private Vector2 inputVelocity;
+    private Vector2 dashVelocity;
+    public float speed;
+    private float speedModifier;
+    private float walkModifier;
+    public float sprintModifier;
+    private bool isSprinting;
+    public bool isCutscene;
+    private float timeCount;
+    private float cooldownCount;
+    public float dashTime;
+    public float dashModifier;
+    public float dashCooldown;
+    private bool isDashing;
+    InputAction moveAction;
+    InputAction sprintAction;
+    InputAction dashAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isSprinting = false;
+        isDashing = false;
+        timeCount = 0;
+        cooldownCount = dashCooldown;
+        walkModifier = 1f;
+        moveAction = InputSystem.actions.FindAction("Move");
+        sprintAction = InputSystem.actions.FindAction("Sprint");
+        dashAction = InputSystem.actions.FindAction("Dash");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!isCutscene)
+        {
+            cooldownCount += Time.deltaTime;
+            if (dashAction.IsPressed() && cooldownCount >= dashCooldown)
+            {
+                cooldownCount = 0;
+                Debug.Log("Dash");
+                isDashing = true;
+            }
+            if (isDashing)
+            {
+                timeCount += Time.deltaTime;
+                if (timeCount < dashTime)
+                {
+                    Vector2 velocity = body.velocity;
+                    dashVelocity = inputVelocity.normalized;
+                    dashVelocity.x *= (speed * dashModifier * Time.deltaTime);
+                    dashVelocity.y *= (speed * dashModifier * Time.deltaTime);
+                    body.AddForce((dashVelocity - (velocity * 16)) * (1/speedModifier));
+                    Debug.Log(velocity);
+                }
+                else
+                {
+                    timeCount = 0;
+                    isDashing = false;
+                }
+            }
+            else
+            {
+                if (sprintAction.IsPressed())
+                {
+                    isSprinting = true;
+                    speedModifier = sprintModifier;
+                }
+                else
+                {
+                    isSprinting = false;
+                    speedModifier = walkModifier;
+                }
+                Vector2 velocity = body.velocity;
+                inputVelocity = moveAction.ReadValue<Vector2>();
+                inputVelocity.x *= (speed * Time.deltaTime * speedModifier);
+                inputVelocity.y *= (speed * Time.deltaTime * speedModifier);
+                body.AddForce(inputVelocity - (velocity * 16));
+                Debug.Log(velocity);
+            }
+        }
+        else
+        {
+            body.velocity = new Vector2(0, 0);
+        }
+    }
+}
