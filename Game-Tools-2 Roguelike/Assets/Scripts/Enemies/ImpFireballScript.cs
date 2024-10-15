611374df1511ec379f93723ddf8cf81ec73700b6
SeamusLynchTUD <c23471656@mytudublin.ie>
2024-10-16 00:23:27 +0100

    Added Imp Animations and Fixed Imp Behaviours

--- Game-Tools-2 Roguelike/Assets/Scripts/Enemies/ImpFireballScript.cs
+++ Game-Tools-2 Roguelike/Assets/Scripts/Enemies/ImpFireballScript.cs
@@ -5,39 +5,82 @@ using UnityEngine;
 
 public class ImpFireballScript : MonoBehaviour
 {
+    public PlayerHealthScript playerHealthScript;
+    public ImpScript impScript;
     public Rigidbody2D body;
     public GameObject player;
+    public GameObject imp;
+    public GameObject fireballVelocityObject;
+    private Vector2 velocity;
+    private Vector2 inputVelocity;
     private Vector2 fireballVelocity;
     public int speed;
     public float nearTurnSpeed;
     public float farTurnSpeed;
+    public float fireballTime;
+    private int impTriggerCount;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Starting velocity: " + body.velocity);
+        impTriggerCount = 0;
+        fireballVelocity = -player.transform.InverseTransformPoint(transform.position);
+        //Debug.Log("Starting velocity: " + body.velocity);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 velocity = body.velocity;
-        Vector2 inputVelocity = -player.transform.InverseTransformPoint(transform.position).normalized;
-        fireballVelocity = fireballVelocity.normalized;
-        /*
-        if ((inputVelocity.x * fireballVelocity.x) < 0)
-        {
-            if (inputVelocity.y > fireballVelocity.y)
-            {
+        //Debug.Log(impTriggerCount);
+        fireballTime += Time.deltaTime;
+        velocity = body.velocity;
+        inputVelocity = -player.transform.InverseTransformPoint(transform.position);
+        fireballVelocity.x = Mathf.MoveTowards(fireballVelocity.x, inputVelocity.x, nearTurnSpeed * Mathf.Abs(inputVelocity.x));
+        fireballVelocity.y = Mathf.MoveTowards(fireballVelocity.y, inputVelocity.y, nearTurnSpeed * Mathf.Abs(inputVelocity.y));
+        fireballVelocityObject.transform.localPosition = fireballVelocity;
+        Vector2 moveVelocity = fireballVelocity.normalized;
+        //Debug.Log("Fireball: " + fireballVelocity);
+        //Debug.Log("Input: " + inputVelocity);
+        //Debug.Log("Move: " + moveVelocity);
+        moveVelocity.x *= (speed * Time.deltaTime);
+        moveVelocity.y *= (speed * Time.deltaTime);
+        body.AddForce(moveVelocity - (velocity * 16));
+    }
 
-            }
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            transform.localPosition = new Vector2(0, 0);
+            inputVelocity = new Vector2(0, 0);
+            fireballVelocity = new Vector2(0, 0);
+            gameObject.SetActive(false);
+            playerHealthScript.playerHealth--;
+            Debug.Log("Player Hit! Health is now: " + playerHealthScript.playerHealth);
+            impScript.fireballActive = false;
         }
-        */
-        fireballVelocity.x = Mathf.MoveTowards(fireballVelocity.x, inputVelocity.x, nearTurnSpeed);
-        fireballVelocity.y = Mathf.MoveTowards(fireballVelocity.y, inputVelocity.y, nearTurnSpeed);
-        Debug.Log("Fireball: " + fireballVelocity);
-        Debug.Log("Input: " + inputVelocity);
-        fireballVelocity.x *= (speed * Time.deltaTime);
-        fireballVelocity.y *= (speed * Time.deltaTime);
-        body.AddForce(fireballVelocity - (velocity * 16));
+        if (collision.gameObject.tag == "Enemy" && fireballTime >= 1)
+        {
+            Destroy(collision.gameObject.transform.parent.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == imp);
+        {
+            //Debug.Log(collision.gameObject.name);
+            impTriggerCount++;
+            //Debug.Log("impTrigger: " + impTriggerCount);
+        }
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == gameObject.transform.parent);
+        {
+            //Debug.Log(collision.gameObject.name);
+            impTriggerCount--;
+            //Debug.Log("impTrigger: "+ impTriggerCount);
+        }
     }
 }
